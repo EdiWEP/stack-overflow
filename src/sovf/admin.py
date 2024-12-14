2e2d72453eb442ceb072850c9b3758d3662d0876
Edi Podaru <edipodaru@gmail.com>
2024-12-14 19:58:03 +0200

    Add models for database entities

--- src/sovf/admin.py
+++ src/sovf/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Question, Answer, Vote, AnswerVote
 
-# Register your models here.
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at', 'vote_count')
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('content', 'author', 'question', 'created_at', 'vote_count')
+
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'question', 'value', 'created_at')
+
+@admin.register(AnswerVote)
+class AnswerVoteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'answer', 'value', 'created_at')
--- src/sovf/models.py
+++ src/sovf/models.py
@@ -1,3 +1,84 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+UPVOTE = 1
+DOWNVOTE = -1
+
+VOTE_CHOICES = [
+    (UPVOTE, 'upvote'),
+    (DOWNVOTE, 'downvote'),
+]
+
+
+class Question(models.Model):
+
+    title = models.CharField(max_length=200)
+    content = models.TextField(max_length=10000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="questions")
+
+    def score(self) -> int:
+        """Returns the question's score: upvotes - downvotes"""
+        upvotes = self.votes.filter(value=UPVOTE).count()
+        downvotes = self.votes.filter(value=DOWNVOTE).count()
+        return upvotes - downvotes
+
+
+class Vote(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='votes',
+    )
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE,
+        related_name='votes',
+    )
+    value = models.SmallIntegerField(choices=VOTE_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'question')
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE,
+        related_name='answers',
+    )
+    content = models.TextField()
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='answers',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def vote_count(self):
+        """Returns the answers's score: upvotes - downvotes"""
+        upvotes = self.votes.filter(value=UPVOTE).count()
+        downvotes = self.votes.filter(value=DOWNVOTE).count()
+        return upvotes - downvotes
+
+class AnswerVote(models.Model):
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='answer_votes',
+    )
+    answer = models.ForeignKey(
+        Answer,
+        on_delete=models.CASCADE,
+        related_name='votes',
+    )
+    value = models.SmallIntegerField(choices=VOTE_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'answer')

897710c10a431817235dceb313dfbc14fb816699
Edi Podaru <edipodaru@gmail.com>
2024-12-18 20:33:56 +0200

    add new models to admin.py

--- src/sovf/admin.py
+++ src/sovf/admin.py
@@ -1,18 +1,26 @@
 from django.contrib import admin
-from .models import Question, Answer, Vote, AnswerVote
+from .models import Question, Answer, Vote, AnswerVote, AnswerComment, AnswerCommentVote
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'created_at')
+    list_display = ("title", "author", "created_at")
 
 @admin.register(Answer)
 class AnswerAdmin(admin.ModelAdmin):
-    list_display = ('content', 'author', 'question', 'created_at')
+    list_display = ("content", "author", "question", "created_at")
+
+@admin.register(AnswerComment)
+class AnswerCommentAdmin(admin.ModelAdmin):
+    list_display = ("content", "author", "answer", "created_at")
 
 @admin.register(Vote)
 class VoteAdmin(admin.ModelAdmin):
-    list_display = ('user', 'question', 'value', 'created_at')
+    list_display = ("user", "question", "value", "created_at")
 
 @admin.register(AnswerVote)
 class AnswerVoteAdmin(admin.ModelAdmin):
-    list_display = ('user', 'answer', 'value', 'created_at')
+    list_display = ("user", "answer", "value", "created_at")
+
+@admin.register(AnswerCommentVote)
+class AnswerCommentVoteAdmin(admin.ModelAdmin):
+    list_display = ("user", "comment", "value", "created_at")
